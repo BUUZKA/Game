35daf758082900eb1d08f52fe691640b8660e531
BUUZKA <72550805+BUUZKA@users.noreply.github.com>
2020-11-15 16:50:06 +0100

    Create rejestracja.php

---
+++ rejestracja.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Osadnicy - zaloz darmowe konto!</title>
+</head>
+
+<body>
+
+  <form method="post">
+  	
+   Nickname: <br /> <input type="text" name="nick" /> <br />
+
+   E-mail: <br /> <input type="text" name="email" /> <br />
+
+   Twoje Haslo: <br /> <input type="password" name="haslo1" /> <br />
+
+   Powtorz Haslo: <br /> <input type="password" name="haslo2" /> <br />
+
+   <input type="checkbox" name="regulamin" /> Akceptuje regulamin
+
+
+
+  </form>
+
+</body>
+</html>
