15441acedaf7cb5788249069821dd359f6fae109
BUUZKA <72550805+BUUZKA@users.noreply.github.com>
2020-11-26 12:34:48 +0100

    Update village.class.php

--- class/village.class.php
+++ class/village.class.php
@@ -9,8 +9,29 @@ class Village
         $this->buildings = array(
             'townHall' => 1,
             'woodcutter' => 1,
+            'ironMine' => 1,
         );
+    $this->storage = array(
+        'wood' => 0,
+        'iron' => 0,
+    );
     }
-
+    private function woodGain(int $deltaTime) : float
+    {
+        $gain = pow($this->buildings['woodcutter'],2) * 250;
+        $perSecondGain = $gain / 3600;
+        return $perSecondGain * $deltaTime;
+    }
+    private function ironGain(int $deltaTime) : float
+    {
+        $gain = pow($this->buildings['ironMine'],2) * 150;
+        $perSecondGain = $gain / 3600;
+        return $perSecondGain * $deltaTime;
+    }
+    public function gain($deltaTime)
+    {
+        $this->storage['wood'] += $this->woodGain($deltaTime);
+        $this->storage['iron'] += $this->ironGain($deltaTime);
+    }
 }
 ?>
