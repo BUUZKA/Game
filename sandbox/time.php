f97f6ca4c57d3bded80ec3eae8e0a42e0b5b1e26
BUUZKA <72550805+BUUZKA@users.noreply.github.com>
2020-12-06 22:12:34 +0100

    Update time.php

--- sandbox/time.php
+++ sandbox/time.php
@@ -1,14 +1,14 @@
 <?php
 $teraz = time();
 echo "teraz jest: ".date('H:i:s d.m.Y', $teraz)."<br>";
-$losowaLiczbaSekund = rand(0,7200); //od 0 sekund do 2 godzin
+$losowaLiczbaSekund = rand(0,7200); 
 $przeszlosc = $teraz - $losowaLiczbaSekund;
 echo "W przeszłości ".date('H:i:s d.m.Y', $przeszlosc)." od tego czasu minęło $losowaLiczbaSekund sekund"."<br>";
 $produkcjaDrewnaGodzine = 1000;
 $produkcja = ($produkcjaDrewnaGodzine / 3600) * $losowaLiczbaSekund;
 echo "Od przeszłości do teraz wyprodukowano ".floor($produkcja)." sztuk drewna<br>";
 
-$czasUlepszeniaBudynku = 2700; //45min
+$czasUlepszeniaBudynku = 2700; 
 
 echo "Rozpoczęto ulepszanie budynku na następny poziom<br>";
 $budynekGotowy = time() + $czasUlepszeniaBudynku;
