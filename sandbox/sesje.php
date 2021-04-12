5cdb396d892932a38cafe040257c0aa3957a72ec
BUUZKA <72550805+BUUZKA@users.noreply.github.com>
2021-04-12 12:52:47 +0200

    Update sesje.php

--- sandbox/sesje.php
+++ sandbox/sesje.php
@@ -1,15 +1,15 @@
 <?php
 session_start();
 
-if( !isset($_SESSION['ostatniRefresh']) ) 
+if( !isset($_SESSION['ostatniRefresh']) ) //sprawdź czy istnieje taka zmienna w sesji
 {
-    $_SESSION['ostatniRefresh'] = time(); 
+    $_SESSION['ostatniRefresh'] = time(); //jeżeli nie istnieje taka zmienna w sesji to zapisz obecny czas
     $_SESSION['skladDrewna'] = 0;
 }
 else
 {
-    $staryCzas = date('H:i:s d.m.Y', $_SESSION['ostatniRefresh']); 
-    $nowyCzas = date('H:i:s d.m.Y'); 
+    $staryCzas = date('H:i:s d.m.Y', $_SESSION['ostatniRefresh']); // czas pierwszego odświeżenia strony
+    $nowyCzas = date('H:i:s d.m.Y'); // czas teraz
     $iloscSekund = time() - $_SESSION['ostatniRefresh'];
     echo "Od $staryCzas do teraz ($nowyCzas) minęło $iloscSekund sekund<br>";
     $produkcjaDrewna = 1000;
